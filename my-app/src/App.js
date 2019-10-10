af41e126d9535f1bd52fd55f748210abcc05c6a4
mi19her <milcaph.19@gmail.com>
2019-10-10 15:55:28 -0500

    pagina de inicio

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+const inicio = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-link">Bienvenidos</h1>
+        <button className= "App-link" id="ingresar"> Ingresar</button>
+      </header>
+    </div>
+  );
+}
+
+export default inicio;
+
+const page2 = () => {
+    // return(
+
+    // );
+}
