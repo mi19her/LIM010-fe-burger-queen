e169fd1fb99d956bb5cab6c85e47baf808528c50
ElizabethTorre <etr604@gmail.com>
2019-10-17 17:57:31 -0500

    LOGO

--- my-app/src/views/welcome.js
+++ my-app/src/views/welcome.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from "../img/logoBurger.png";
 export const Welcome =()=>{
     return (
      <div className="App">
        <header className="App-header">
        {/* <h1 className="App-link">Bienvenidos</h1> */}
-       <img src="../img/logoBurger.png" alt={"imagen de product"} className="Width-product"/>
+       <img src={logo} alt="imagen de product"/>
        <button className= "" id="ingresar"><Link to="/waiter"> Mesero </Link></button>
        </header>
      </div>
