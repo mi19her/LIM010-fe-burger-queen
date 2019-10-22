3f93c9fd34f5a28b067621b4c63d5bd56d768e69
ElizabethTorre <etr604@gmail.com>
2019-10-22 18:05:16 -0500

    Cantidad

--- my-app/src/components/Order.jsx
+++ my-app/src/components/Order.jsx
@@ -1,35 +1,46 @@
 import React from "react";
-
+// import "";
 const { useState } = React;
 
-export const Order = ({ products }) => {
+export const Order = ({ products, cantidad }) => {
   const [name, setName] = useState('');
+  // const [cant, setCant] = useState('1');
   const functionName = (e) => {
     setName(e.target.value);
     console.log(e.target.value);
   }
+  // const functionCant = (e) => {
+  //   setCant(e.target.value);
+  //   // console.log(e.target.value);
+  // }
   return (
     <div>
-      <input placeholder="Nombre" value={name} onChange={functionName}></input>
+      <input placeholder="Nombre" value={name} onChange={functionName}/>
       <input placeholder="N° de Mesa"></input>
       <p>Lista de Pedidos</p>
       <table>
         <thead>
           <tr>
-            <td>Cantidad</td>
-            <td>Nombre</td>
-            <td>Precio</td>
+            <td>CANT.</td>
+            <td>DESCRIPCIÓN</td>
+            <td>PU</td>
+            <td>TOTAL</td>
           </tr>
         </thead>
+        <tbody >
         {products.map(product => (
-          <tbody  key={product.id}>
-            <tr>
-              <td><input type="number" min="1" max="100" defaultValue="1"/></td>
+            <tr key={product.id}>
+              <td><input type="number" min="1" max="100" defaultValue={product.cantidad} onChange={(e) => {
+              const p = e.target.value;
+              cantidad(product.id, p)
+              console.log(e.target.value)
+              }}/></td>
               <td>{product.nombre}</td>
               <td>{product.precio}</td>
+              <td>{product.precio* product.cantidad }</td>
             </tr>
-          </tbody>
         ))}
+        </tbody>
       </table>
     </div>
   )
--- my-app/src/views/waiter.js
+++ my-app/src/views/waiter.js
@@ -2,9 +2,22 @@ import React, {useState} from "react";
 import logo from "../img/logoBurger.png";
 import { Products } from '../components/Products';
 import { Order } from '../components/Order';
+import "../css/App.css";
 
 export const Waiter = () => {
   const [selectProduct, setOrder] = useState([]);
+  const incrementar = (prodId, cant = 1) => {
+    console.log(cant, prodId)
+    let newcant = '';
+    selectProduct.map(element =>{
+      if(prodId === element.id) {
+       element.cantidad = cant;
+        console.log('hola', element.cantidad)      
+      }
+      newcant= element.cantidad;
+    })
+    return newcant;
+  }
 
     return(
         <div>
@@ -15,9 +28,9 @@ export const Waiter = () => {
             <main>
               <section className="Flex">
                 <Products addProductAtOrder={(product) => {
-                  setOrder([...selectProduct,product])
+                  setOrder([...selectProduct, { ...product, cantidad:1}])
                 }}/>
-                  <Order products={selectProduct}/>
+                  <Order products={selectProduct} cantidad={incrementar}/>
               </section>
             </main>
         </div>
