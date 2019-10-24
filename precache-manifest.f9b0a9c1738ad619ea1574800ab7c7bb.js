b6f0c8b3e540823d662969e3986840365195ec4e
mi19her <milcaph.19@gmail.com>
2019-10-24 16:40:21 -0500

    Updates

---
+++ precache-manifest.f9b0a9c1738ad619ea1574800ab7c7bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4647dea31002f17cf65155d2a45f86",
+    "url": "/LIM010-fe-burger-queen/my-app/index.html"
+  },
+  {
+    "revision": "46ba8e69a82ae925cd6d",
+    "url": "/LIM010-fe-burger-queen/my-app/static/css/main.12b8e8bd.chunk.css"
+  },
+  {
+    "revision": "ece0a036b776ea6ce6f2",
+    "url": "/LIM010-fe-burger-queen/my-app/static/js/2.c2742071.chunk.js"
+  },
+  {
+    "revision": "46ba8e69a82ae925cd6d",
+    "url": "/LIM010-fe-burger-queen/my-app/static/js/main.ed73f257.chunk.js"
+  },
+  {
+    "revision": "648d269aec8ddb36c251",
+    "url": "/LIM010-fe-burger-queen/my-app/static/js/runtime-main.48f27612.js"
+  },
+  {
+    "revision": "eda230cd5774eb5998c2cada266429c8",
+    "url": "/LIM010-fe-burger-queen/my-app/static/media/logoBurger.eda230cd.png"
+  }
+]);
